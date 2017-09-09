0dd7b5c23be696449af30c010a84b4011946ec65
Chris Nady <nady.christopher@gmail.com>
2017-09-09 19:01:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count.has_key?(:genre)
+      if genre_count.has_key?("genre")
         genre_count(genre: value += 1)
       else
         genre_count(genre: 1)
